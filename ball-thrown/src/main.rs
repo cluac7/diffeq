c6241f9eb45e58a02580efae74160e8c51109abc
Rahul Saini <rahul.saini@education.nsw.gov.au>
2024-05-29 01:13:52 +1000

    initial commit

---
+++ ball-thrown/src/main.rs
@@ -0,0 +1,48 @@
+use std::io::stdin;
+
+// Constants
+const G: f64 = -9.8;
+
+fn height(time: f64, accuracy: f64, starting_vel: f64) -> f64 {
+    let mut height: f64 = 0.0;
+    let mut vel: f64 = starting_vel;
+    let timestep = 1.0/accuracy;
+
+    let steps: u64 = (time / timestep) as u64;
+    
+    for _n in 0..steps {
+        height += vel * timestep;
+        vel += G * timestep;
+    }
+    
+    if height >= 0.0 {height} else {0.0}
+}
+
+fn main() {
+    let mut time = String::new();
+    let mut accuracy = String::new();
+    let mut starting_vel = String::new();
+
+    println!("Enter the total time (s), then desired accuracy (s^-1), then the starting velocity (ms^-1): ");
+
+    stdin()
+        .read_line(&mut time)
+        .ok()
+        .expect("failed to read total time");
+    stdin()
+        .read_line(&mut accuracy)
+        .ok()
+        .expect("failed to read total time");
+    stdin()
+        .read_line(&mut starting_vel)
+        .ok()
+        .expect("failed to read total time");
+
+    let final_height = height(
+        time.trim().parse::<f64>().unwrap(),
+        accuracy.trim().parse::<f64>().unwrap(),
+        starting_vel.trim().parse::<f64>().unwrap(),
+    );
+
+    println!("The final height is {final_height}");
+}
---
+++ pendulum/pendulum.cpp
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <iostream>
+#include <numbers>
+
+// Physical Consts
+constexpr double g = 9.81;
+constexpr double L = 2.0;
+constexpr double mu = 0.1;
+
+constexpr double THETA_0=std::numbers::pi/3;
+constexpr double THETA_DOT_0 = 0.0;
+
+auto get_theta_ddot(double theta, double theta_dot) -> double{
+  return -mu * theta_dot - (g/L) * std::sin(theta);
+}
+
+// solution to equation
+auto theta(double time, double timestep) -> double {
+  double theta = THETA_0;
+  double theta_dot = THETA_DOT_0;
+
+  for (double i=timestep; i <= time; i+=timestep) {
+    theta+=theta_dot*timestep;
+    theta_dot+=get_theta_ddot(theta, theta_dot)*timestep;
+  }
+  
+  return theta;
+}
+
+int main() {
+  std::cout << "enter a total time and timestep: " << std::endl;
+  double total_time, timestep;
+  std::cin >> total_time >> timestep;
+
+  std::cout << "theta is: " << theta(total_time, timestep);
+
+  return 0;
+}
